bb125c875dfcb0cc8be3f7a75c2a15c6f8357986
Leo <106483107+LeoGsky@users.noreply.github.com>
2022-08-30 02:37:00 +0800

    Add files via upload

---
+++ Module-17.HW/Module-17.HW/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Module-17.HW
+//
+//  Created by Леонид Габзовский on 2022/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberZero: UIButton!
+    
+    @IBOutlet weak var numberOne: UIButton!
+    
+    @IBOutlet weak var numberTwo: UIButton!
+    
+    @IBOutlet weak var numberThree: UIButton!
+    
+    @IBOutlet weak var numberFour: UIButton!
+    
+    @IBOutlet weak var numberFive: UIButton!
+    
+    @IBOutlet weak var numberSix: UIButton!
+    
+    @IBOutlet weak var numberSeven: UIButton!
+    
+    @IBOutlet weak var numberEight: UIButton!
+    
+    @IBOutlet weak var numberNine: UIButton!
+    
+    @IBOutlet weak var fraction: UIButton!
+    
+    @IBOutlet weak var positiveOrNegative: UIButton!
+    
+    @IBOutlet weak var percentage: UIButton!
+    
+    @IBOutlet weak var delete: UIButton!
+    
+    @IBOutlet weak var division: UIButton!
+    
+    @IBOutlet weak var multiplication: UIButton!
+    
+    @IBOutlet weak var minus: UIButton!
+    
+    @IBOutlet weak var plus: UIButton!
+    
+    @IBOutlet weak var equal: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
